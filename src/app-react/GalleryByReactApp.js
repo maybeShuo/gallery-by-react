2b549ded690e65ac724e51df6db5e8638fd8328b
I330045 <shuo.chen01@sap.com>
2016-07-04 15:42:27 +0800

    Add img.

--- src/app-react/GalleryByReactApp.js
+++ src/app-react/GalleryByReactApp.js
@@ -3,18 +3,41 @@
 const imageData = require("../data/imageData.json");
 
 //生成图片路径
-function genImageURL(){
+function genImageURL() {
     imageData.map((item) => {
         item.imageURL = "assets/images/" + item.fileName;
     });
 }
 genImageURL();
 
+let ImgFigure = React.createClass({
+    render: function() {
+        return (
+            <figure className="img-figure">
+                <img src={this.props.data.imageURL}
+                     alt={this.props.data.title}
+                />
+                <figcaption>
+                    <h2 className="img-title">{this.props.data.title}</h2>
+                </figcaption>
+            </figure>
+        );
+    }
+});
+
 const GalleryByReactApp = React.createClass({
     render: function() {
+        let controllerUnits = [],
+            imgFigures = [];
+
+        imageData.map((item) => {
+            imgFigures.push(<ImgFigure data={item}>);
+        });
+
         return (
             <section className="stage">
                 <section className="img-sec">
+                    {imgFigures}
                 </section>
                 <nav className="controller-nav">
                 </nav>
--- src/app/GalleryByReactApp.js
+++ src/app/GalleryByReactApp.js
@@ -3,15 +3,31 @@
 const imageData = require("../data/imageData.json");
 
 //生成图片路径
-function genImageURL(){
+function genImageURL() {
     imageData.map((item) => {
         item.imageURL = "assets/images/" + item.fileName;
     });
 }
 genImageURL();
 
+let ImgFigure = React.createClass({displayName: "ImgFigure",
+    render: function() {
+        return (
+            React.createElement("figure", null, 
+                React.createElement("img", null), 
+                React.createElement("figcaption", null, 
+                    React.createElement("h2", null)
+                )
+            )
+        );
+    }
+});
+
 const GalleryByReactApp = React.createClass({displayName: "GalleryByReactApp",
     render: function() {
+        let controllerUnits = [],
+            ImgFigures = [];
+
         return (
             React.createElement("section", {className: "stage"}, 
                 React.createElement("section", {className: "img-sec"}
